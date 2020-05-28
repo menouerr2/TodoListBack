ddf16a934a7a7a6b792ad8570a82d819f32aac8e
MENOUER1 <menouerr2@gmail.com>
2020-05-28 21:45:28 +0200

    change findAll to findOne in todoItems controllers

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+const router = express.Router();
+
+const todosController = require('../server/controllers').todos;
+const todoItemsController = require('../server/controllers').todoItems;
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/api', (req, res) => res.status(200).send({
+  message: 'Welcome to the Todos API!',
+}));
+
+router.post('/api/todos', todosController.create);
+
+router.get('/api/todos', todosController.list);
+
+router.post('/api/todos/:todoId/items', todoItemsController.create);
+
+router.get('/api/todos/:todoId', todosController.retrieve);
+
+router.put('/api/todos/:todoId', todosController.update);
+
+router.delete('/api/todos/:todoId', todosController.destroy);
+
+router.put('/api/todos/:todoId/items/:todoItemId', todoItemsController.update);
+router.delete(
+  '/api/todos/:todoId/items/:todoItemId', todoItemsController.destroy
+);
+router.all('/api/todos/:todoId/items', (req, res) =>
+  res.status(405).send({
+    message: 'Method Not Allowed',
+  }));
+module.exports = router;
